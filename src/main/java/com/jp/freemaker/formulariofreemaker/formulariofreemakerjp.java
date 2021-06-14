80cad01807cc6516a34b671687a30dba8c0b355f
unknown <joaopedrodinizferreira@gmail.com>
2021-06-13 23:57:29 -0300

    ok

---
+++ src/main/java/com/jp/freemaker/formulariofreemaker/formulariofreemakerjp.java
@@ -0,0 +1,13 @@
+package com.jp.freemaker.formulariofreemaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class formulariofreemakerjp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(formulariofreemakerjp.class, args);
+	}
+
+}
---
+++ src/test/java/com/jp/freemaker/formulariofreemaker/Free1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jp.freemaker.formulariofreemaker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class formulariofreemakerjp {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
